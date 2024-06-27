8a7a695bf7f188eb78a2b9297645f70d7fd66b8a
Pratik Shukla <79405008+Mr-incognito1109@users.noreply.github.com>
2024-06-27 11:52:14 +0530

    fixed loop issues

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Print welcome message and menu
         welcomeMessage.printWelcomeMessage();
         menu();
 
