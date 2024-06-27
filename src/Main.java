a66531bda0a8490ec8e162b9b18125654d543cb4
Pratik Shukla <79405008+Mr-incognito1109@users.noreply.github.com>
2024-06-27 11:46:27 +0530

    added welcomeMessage

--- src/Main.java
+++ src/Main.java
@@ -32,23 +32,16 @@ public class Main {
                 }
 
                 if (choice == 4) {
-                    // If the choice is to exit, break out of the loop
                     break;
                 } else {
-                    // If not, display the menu again
                     menu();
                 }
             } else {
-                // Invalid choice
                 System.out.println("Invalid choice. Please try again.");
             }
         }
-
-        // Close the scanner
         in.close();
     }
-
-    // Function to display the menu
     public static void menu() {
         System.out.print("\033[1m"); // Enable bold style
         System.out.println("\nMenu:");
@@ -62,50 +55,44 @@ public class Main {
     }
 
     public static void celsiusToAll(){
-        // Get the temperature in Celsius
+
         System.out.println("Enter the temperature in Celsius: ");
         Scanner input = new Scanner(System.in);
         double celsius = input.nextDouble();
         input.close();
 
-        // Convert Celsius to Fahrenheit
         double fahrenheit = (celsius * 9/5) + 32;
         System.out.println("Temperature in Fahrenheit: " + fahrenheit);
 
-        // Convert Celsius to Kelvin
         double kelvin = celsius + 273.15;
         System.out.println("Temperature in Kelvin: " + kelvin);
 
     }
 
     public static void fahrenheitToAll(){
-        // Get the temperature in Fahrenheit
+
         System.out.println("Enter the temperature in Fahrenheit: ");
         Scanner input = new Scanner(System.in);
         double fahrenheit = input.nextDouble();
         input.close();
 
-        // Convert Fahrenheit to Celsius
         double celsius = (fahrenheit - 32) * 5/9;
         System.out.println("Temperature in Celsius: " + celsius);
 
-        // Convert Fahrenheit to Kelvin
         double kelvin = (fahrenheit - 32) * 5/9 + 273.15;
         System.out.println("Temperature in Kelvin: " + kelvin);
     }
 
     public static void kelvinToAll(){
-        // Get the temperature in Kelvin
+
         System.out.println("Enter the temperature in Kelvin: ");
         Scanner input = new Scanner(System.in);
         double kelvin = input.nextDouble();
         input.close();
 
-        // Convert Kelvin to Celsius
         double celsius = kelvin - 273.15;
         System.out.println("Temperature in Celsius: " + celsius);
 
-        // Convert Kelvin to Fahrenheit
         double fahrenheit = (kelvin - 273.15) * 9/5 + 32;
         System.out.println("Temperature in Fahrenheit: " + fahrenheit);
     }
